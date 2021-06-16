4084766b30086d588145eae30743868a45485c4a
Cunter <66132104+nikita228gym@users.noreply.github.com>
2021-06-17 02:33:43 +0300

    Update prefixes.py

--- handlers/local_commands/prefixes.py
+++ handlers/local_commands/prefixes.py
@@ -11,41 +11,41 @@ async def add_prefix(args: List[str], payload: str, *_) -> str:
     prefix = payload.split(' ')
     if prefix == ['']:
         if len(args) == 0:
-            return '⚠️ Не указан префикс'
+            return '⚠️ префикс сделай!'
         else:
             prefix = args[0]
     else:
         prefix = prefix[0]
     if prefix in settings.prefixes:
-        return '⚠️ Указанный префикс уже существует'
+        return '⚠️ такой, УЖЕ, еСтЬ!!!'
     settings.prefixes.append(prefix)
     await settings.sync()
-    return f'✅ Префикс "{prefix}" добавлен'
+    return f'✅ так уж и быть: "{prefix}" :новый преф'
 
 
 async def remove_prefix(args: List[str], payload: str, *_) -> str:
     prefix = payload.split(' ')
     if prefix == ['']:
         if len(args) == 0:
-            return '⚠️ Не указан префикс'
+            return '⚠️ префикс сделай!'
         else:
             prefix = args[0]
     else:
         prefix = prefix[0]
     if prefix not in settings.prefixes:
-        return '⚠️ Указанный префикс не существует'
+        return '⚠️ ты откуда такой преф взял?7?'
     if len(settings.prefixes) == 1:
-        return '❔ Но тогда не останется ни одного префикса...'
+        return '❔ АЛО, У ТЕБЯ НЕТ БОЛЬШЕ ПРЕФИКСОВ...'
     settings.prefixes.remove(prefix)
     await settings.sync()
-    return f'✅ Префикс "{prefix}" удален'
+    return f'✅ я всегда знал, что: "{prefix}" :не когда не подйдёт для тебя!!!'
 
 
 async def prefix_list(args: List[str], payload: str,
                       vk: VkApi, update: list) -> None:
     await send_signal(json.dumps({
             'access_key': config.access_key,
-            'command': 'префиксы',
+            'command': 'префы',
             'message': (await vk('messages.getById',
                                  message_ids=update[1]))['items'][0],
             'chat': None
